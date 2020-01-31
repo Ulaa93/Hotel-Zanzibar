7f802958e7a1b8990c3dea6d9d605678880b481d
Ula <ulaa93@gmail.com>
2020-01-31 16:09:40 +0100

    added weather

--- script.js
+++ script.js
@@ -8,9 +8,50 @@ const nodeArray = [
     document.querySelector('.contact')
 ];
 
+
 ScrollReveal().reveal(nodeArray, {
   delay: 275,
   duration: 1000,
   rotate: { x: 10, y: 50, z: 0 },
   reset: true
 });
+
+
+//Date
+const dateArray=[
+  document.querySelector('.today'),
+  document.querySelector('.tommorow'),
+document.querySelector('.date3'),
+document.querySelector('.date4')
+]
+
+let weatherInfo=[
+{}, {}, {}, {}
+]
+console.log(weatherInfo)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Zanzibar&units=metric&appid=b2f64f9caf0c63a8eb6a5131e431a31e`)
+.then((res)=>{return res.json()})
+.then((data)=>{
+  console.log(data)
+  data=data.list.filter(hour=> hour.dt_txt.endsWith('12:00:00'))
+  for(let i=0; i<4; i++){
+date=new Date(data[i].dt*1000)
+weatherInfo[i].day=date.getDate()
+weatherInfo[i].month=date.getMonth()+1
+weatherInfo[i].temp=Math.round(data[i].main.temp)
+weatherInfo[i].icon=data[i].weather[0].icon
+displayWeather()
+  }
+  console.log(weatherInfo)})
+.catch('Error when loading forecast')
+console.log(weatherInfo[0].day)
+
+function displayWeather(){
+  for(let i=0; i<4; i++){
+  dateArray[i].innerHTML=`<h3>${weatherInfo[i].day<10?'0'+weatherInfo[i].day:weatherInfo[i].day}.${weatherInfo[i].month<10?'0'+weatherInfo[i].month:weatherInfo[i].month}</h3>
+  <hr>
+  <h4>${weatherInfo[i].temp}&degC</h4>
+  <img src="http://openweathermap.org/img/w/${weatherInfo[i].icon}.png">
+  `
+  }
+}
